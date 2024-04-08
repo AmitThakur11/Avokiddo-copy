863a7d748c381dc221f0aa28232200baa414cc18
Amit Thakur <amithakur081@gmail.com>
2024-04-08 15:44:43 +0530

    add button for openeing app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import "./App.css";
 import { useEffect } from "react";
 
 function App() {
-  useEffect(() => {
-    window.location.href("minis://abc?ab=100");
-  });
   return (
     <div className="App">
       <h1>Mini's</h1>
+      <a href="minis://abc?ab=100">Open</a>
     </div>
   );
 }
