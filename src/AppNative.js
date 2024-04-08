cb80f86648881b142e639cb505b9d3e7b0f27678
Amit Thakur <amithakur081@gmail.com>
2024-04-08 16:23:11 +0530

    router dom addedd

--- src/AppNative.js
+++ src/AppNative.js
@@ -2,7 +2,6 @@ import React from "react";
 import { useParams } from "react-router-dom";
 export default function AppNative() {
   const params = useParams();
-  console.log("params--->", params);
   return (
     <div>
       <h1>Mini's</h1>
