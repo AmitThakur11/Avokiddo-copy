b5aeda7af6f158bdab22812099477d938adaef07
Amit Thakur <amithakur081@gmail.com>
2024-04-08 16:33:07 +0530

    minor change

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  createBrowserRouter,
+} from "react-router-dom";
 import Home from "./Home";
 import AppNative from "./AppNative";
+
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<AppNative />} />
           <Route path="/app/:plattform/:code" element={<AppNative />} />
+          <Route path="*" element={<AppNative />} />
         </Routes>
       </Router>
     </div>
