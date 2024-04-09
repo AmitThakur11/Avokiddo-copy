1d5b297708716285764bd370c7919516b269c20b
Amit Thakur <amithakur081@gmail.com>
2024-04-09 16:16:51 +0530

    minor url change

--- src/AppNative.js
+++ src/AppNative.js
@@ -3,7 +3,7 @@ export default function AppNative() {
   return (
     <div>
       <h1>Mini's</h1>
-      <a href="market://play.google.com/store/apps">
+      <a href="market://details?id=com.android.vending">
         <button onClick={() => {}}>Play Store</button>
       </a>
       <a href="itms-apps://play.google.com/store/apps">
