96562c1964339e60f3367ebf5de88703ad260bbe
Amit Thakur <amithakur081@gmail.com>
2024-04-09 16:00:48 +0530

    add playstore link

--- src/AppNative.js
+++ src/AppNative.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 export default function AppNative() {
   return (
     <div>
       <h1>Mini's</h1>
-      <button onClick={() => {}}>Play Store</button>
-      <button onClick={() => {}}>App Store</button>
+      <a href="itms-apps://play.google.com/store/apps">
+        <button onClick={() => {}}>Play Store</button>
+      </a>
+      <a href="itms-apps://apps.apple.com/app">
+        <button onClick={() => {}}>App Store</button>
+      </a>
     </div>
   );
 }
