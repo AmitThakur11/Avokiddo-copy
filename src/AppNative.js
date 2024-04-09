323ccd0cc7ed80393a8f98f491912f4305eafc1e
Amit Thakur <amithakur081@gmail.com>
2024-04-09 16:14:07 +0530

    play store link added

--- src/AppNative.js
+++ src/AppNative.js
@@ -3,10 +3,10 @@ export default function AppNative() {
   return (
     <div>
       <h1>Mini's</h1>
-      <a href="itms-apps://play.google.com/store/apps">
+      <a href="market://play.google.com/store/apps">
         <button onClick={() => {}}>Play Store</button>
       </a>
-      <a href="itms-apps://apps.apple.com/app">
+      <a href="itms-apps://play.google.com/store/apps">
         <button onClick={() => {}}>App Store</button>
       </a>
     </div>
