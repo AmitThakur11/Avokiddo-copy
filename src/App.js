d2974520522c5fe2b77cc82ce10361fa8e7ad47a
Amit Thakur <amithakur081@gmail.com>
2025-01-02 18:53:12 +0530

    add cancellation and refund page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Invite from "./components/Invite/Invite";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import IframeComp from "./IframeComp";
 import PrivacyPolicy from "./PrivacyPolicy";
+import RefundAndCancellation from "./RefundAndCancellation";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Invite />} />
           <Route path="/privacypolicy" element={<PrivacyPolicy />} />
+          <Route path="/refundAndPolicy" element={<RefundAndCancellation />} />
           <Route path="/app/:code" element={<Invite />} />
           <Route path="*" element={<Invite />} />
           <Route path="/iframe" element={<IframeComp />} />
